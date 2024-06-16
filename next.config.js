a4af190895acd26b6a2068895bdc6ef64d80424b
slimanePLF <slimaneber2001@gmail.com>
2024-06-16 11:16:12 +0100

    Fixing deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "build", // <=== enables static exports
+  // basePath: "/portfolio",
+};
 
 module.exports = nextConfig
